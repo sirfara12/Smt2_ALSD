5ecca3cf24f5d2d137d208d696279b293f1fe4d9
sirfara <you@msirvaratigmail.com>
2024-02-27 14:38:31 +0700

    Jobsheet2

---
+++ smt2/PT2/DragonMain.java
@@ -0,0 +1,12 @@
+package PT2;
+
+public class DragonMain {
+        public static void main(String[] args) {
+            Dragon dragon1 = new Dragon(18, 5, 18, 20);
+            dragon1.meveleft();
+            dragon1.moveRight();
+            dragon1.moveUp();
+            dragon1.moveDown();
+            dragon1.printPosition();
+        }
+    }
